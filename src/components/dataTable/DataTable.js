7ee0b84e39817095532c9cbed6c5bf11f9659042
Hunda Guluma <hundaguluma@gmail.com>
2023-01-05 14:27:52 +0300

    export filtered items not all data

--- src/components/dataTable/DataTable.js
+++ src/components/dataTable/DataTable.js
@@ -143,7 +143,7 @@ const DataTable = (_props) => {
     () => (
       <Export
         onExport={() => {
-          downloadCSV(data, sb);
+          downloadCSV(filteredItems, sb);
         }}
       />
     ),
