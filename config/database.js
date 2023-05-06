dfd8ec88d100d62e31a23eaaf902aa3f181004cb
Beharudin <beharudinmm@gmail.com>
2023-05-06 08:15:19 +0300

    some changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 // dotenv.config();
 
 const pool = mysql.createPool({
-  host: '192.168.57.216',
+  host: 'localhost',
+  // host: '192.168.77.225',
   user: 'root',
   password: '',
   database: "bakkalcha",
