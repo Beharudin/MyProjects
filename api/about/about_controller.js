af6a753d7ecdb75e30283d1127244c5289ade383
Beharudin <beharudinmm@gmail.com>
2023-05-06 09:07:30 +0300

    some changes

--- api/about/about_controller.js
+++ api/about/about_controller.js
@@ -9,6 +9,7 @@ export function getAbout(req, res) {
         success: 0,
         message: "failed to fetch about",
         status: "failed",
+        error:error,
       });
     }
     if (!results) {
