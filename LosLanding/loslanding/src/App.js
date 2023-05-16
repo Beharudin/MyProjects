36ad61cbde3f8c7aeb7585609b872ea788f86df0
Beharudin <beharudinmm@gmail.com>
2023-05-16 09:36:20 +0300

    some changes

--- LosLanding/loslanding/src/App.js
+++ LosLanding/loslanding/src/App.js
@@ -1,11 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
-import Topbar from "./components/Topbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <Topbar /> */}
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
