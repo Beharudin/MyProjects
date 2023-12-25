005ee7670e1ed48fcaf1f47c9298308405f63a7f
Beharudin <beharudinmm@gmail.com>
2023-12-25 09:51:18 +0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
             <Route path="/novel/:id" exact element={<ReadNovel />} />
             <Route path="/poem/:id" exact element={<ReadPoem />} />
             <Route path="/post/:id" exact element={<ReadPost />} />
+            <Route path="/hello" element={<Dashboard />} />
 
             <Route
               path="/admin/*"
