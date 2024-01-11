7131e445eecfae54f0afd10b964a4b30156d82ab
Beharudin Mohammed Musa <beharudinm@coopbank.local>
2024-01-11 15:49:28 +0300

    Added styling for active link and prisma initialising

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,35 @@
+"use client";
+import classnames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-import {AiFillBug} from 'react-icons/ai'
+import { AiFillBug } from "react-icons/ai";
 
 const Navbar = () => {
-    const links=[
-        {label: 'Dashboard',href:'/'},
-        {label: 'Issues',href:'/issues'},
-    ]
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 px-5 mb-5 border-b h-14 items-center">
-      <Link href="/"><AiFillBug /></Link>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        {links.map(link=><Link className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link>)}
-        
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
